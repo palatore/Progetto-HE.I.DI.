02c4a0ef66783a0b5f03bebc9dbe8c36a79f39da
Albedo <waforumo177@gmail.com>
2025-05-08 15:34:52 +0200

    Prime modifiche layout tab1 prova

--- HEIDI/src/app/tab1/tab1.page.ts
+++ HEIDI/src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonGrid, IonRow, IonCol, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonGrid, IonRow, IonCol, IonItem],
 })
 export class Tab1Page {
   constructor() {}
+
+  name: string = 'Allah';
 }
