4807081c85966847d3c1a9109acb78c7f78c9d1f
Albedo <waforumo177@gmail.com>
2025-05-08 17:20:16 +0200

    Modifiche di hub provvisorio da mostrare al professore a lezione per far capire a grandi linee cosa si realizza

--- HEIDI/src/app/tab1/tab1.page.ts
+++ HEIDI/src/app/tab1/tab1.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonGrid, IonRow, IonCol, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle, IonGrid, IonRow, IonCol, IonIcon, IonDatetime } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonGrid, IonRow, IonCol, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonGrid, IonRow, IonCol, IonIcon, IonDatetime, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader],
 })
 export class Tab1Page {
   constructor() {}
--- HEIDI/src/app/tab2/tab2.page.ts
+++ HEIDI/src/app/tab2/tab2.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonItem, IonLabel, IonButton, IonCardContent, IonList, IonSearchbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonItem, IonLabel, IonButton, IonList, IonSearchbar],
 })
 export class Tab2Page {
 
