67ad0af563dc8c065ff78f4769b2c284b376aaa2
Albedo <waforumo177@gmail.com>
2025-05-10 14:48:26 +0200

    Creato servizio login, definita bozza pagina login

--- HEIDI/src/app/pages/auth/login/login.page.ts
+++ HEIDI/src/app/pages/auth/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
+import { FormGroup, FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonRow, IonGrid, IonCol, IonCardTitle, IonCardHeader, IonCard, IonLabel, IonInput, IonButton, IonItem, IonCardContent } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 
 @Component({
@@ -9,17 +9,31 @@ import { addIcons } from 'ionicons';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonIcon, IonGrid, IonRow, IonCol, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonLabel, IonInput, IonButton, IonItem]
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
     addIcons({
-      'a': 'assets/icon/favicon.png',
+      'a': 'assets/icon/mountain_line_art.svg',
     });
    }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form inviato!', this.loginForm.value);
+      // Perform login action here
+    } else {
+      console.log('Il form non è valido');
+    }
   }
 
 }
