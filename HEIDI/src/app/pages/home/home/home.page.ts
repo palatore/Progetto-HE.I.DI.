966b7f11ab6d2290cbc7b3cb879bdf0753284ab3
Albedo <waforumo177@gmail.com>
2025-05-12 15:32:34 +0200

    abbozzata home

--- HEIDI/src/app/pages/home/home/home.page.ts
+++ HEIDI/src/app/pages/home/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButton, IonContent, IonHeader, IonItem, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonGrid, IonHeader, IonItem, IonRow, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { LoginService } from 'src/app/services/auth/login.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { LoginService } from 'src/app/services/auth/login.service';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonItem]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonGrid, IonRow]
 })
 export class HomePage implements OnInit {
 
