e98b6de74d8b679ba19ac8b8c242158d5ed9d06d
Albedo <waforumo177@gmail.com>
2026-05-26 21:21:24 +0200

    indentazione

--- HEIDI/src/app/pages/utente/creazione-pasto/riempi-dettagli/riempi-dettagli.component.ts
+++ HEIDI/src/app/pages/utente/creazione-pasto/riempi-dettagli/riempi-dettagli.component.ts
@@ -38,29 +38,29 @@ export class RiempiDettagliComponent  implements OnInit {
   constructor(private formbuilder:FormBuilder, private foodService:GestionePastiService) {
 
     this.riempiDettagliForm = this.formbuilder.group({
-    //per prima cosa passiamo l'id del pasto o dell'allenamento che vogliamo riempire
-    id_attivita: [null, Validators.required],
-    dettaglio_0: null, //questo può essere un alimento o un esercizio in base a cosa vuole costruire l'utente
-    qta_0: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_1: null,
-    qta_1: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_2: null,
-    qta_2: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_3: null,
-    qta_3: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_4: null,
-    qta_4: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_5: null,
-    qta_5: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_6: null,
-    qta_6: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_7: null,
-    qta_7: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_8: null,
-    qta_8: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-    dettaglio_9: null,
-    qta_9: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
-  });
+      //per prima cosa passiamo l'id del pasto o dell'allenamento che vogliamo riempire
+      id_attivita: [null, Validators.required],
+      dettaglio_0: null, //questo può essere un alimento o un esercizio in base a cosa vuole costruire l'utente
+      qta_0: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_1: null,
+      qta_1: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_2: null,
+      qta_2: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_3: null,
+      qta_3: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_4: null,
+      qta_4: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_5: null,
+      qta_5: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_6: null,
+      qta_6: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_7: null,
+      qta_7: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_8: null,
+      qta_8: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+      dettaglio_9: null,
+      qta_9: [null, [Validators.pattern(/^\d{0,4}(\.\d{1,2})?$/)]],
+    });
   }
 
   ngOnInit() {
@@ -73,7 +73,7 @@ export class RiempiDettagliComponent  implements OnInit {
   }
   onImgError(event: any) {
   event.target.src = 'assets/dettagli/default.png';
-}
+  }
 
   segnaDettaglio(id_dettaglio:Number) {
     console.error('CHE FIGATA');
