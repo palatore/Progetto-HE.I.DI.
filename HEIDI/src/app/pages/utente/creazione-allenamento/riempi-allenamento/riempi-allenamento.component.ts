4e7c7a42d3ad4b5967b2a75c65b79668a3ead37a
Paolosalvatore Piazza <92514186+palatore@users.noreply.github.com>
2026-05-27 19:12:26 +0200

    Creata sezione per riempire i dettagli dell'allenamento

---
+++ HEIDI/src/app/pages/utente/creazione-allenamento/riempi-allenamento/riempi-allenamento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { IonCard, IonGrid, IonRow, IonCol, IonItem, IonSelect, IonSelectOption } from "@ionic/angular/standalone";
+
+@Component({
+    selector: 'app-riempi-allenamento',
+    templateUrl: './riempi-allenamento.component.html',
+    styleUrls: ['./riempi-allenamento.component.scss'],
+    imports: [IonItem, IonCol, IonRow, IonGrid, IonCard, IonSelect, IonSelectOption]
+
+})
+export class RiempiAllenamentoComponent {
+    
+}
