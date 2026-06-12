944a2631a78cab48bd0c5ce9df609362c12cac33
Albedo <waforumo177@gmail.com>
2026-06-12 22:17:12 +0200

    sistemati import

--- HEIDI/src/app/pages/home/home/home.page.ts
+++ HEIDI/src/app/pages/home/home/home.page.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonRow, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { LoginService } from 'src/app/services/auth/login.service';
-import { GestionePastiService } from 'src/app/services/pasti/gestione-pasti.service';
 import { map, Observable } from 'rxjs';
 import { RouterModule } from '@angular/router';
 
