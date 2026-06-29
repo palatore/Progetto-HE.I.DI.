88ce8997b0591cac914a7e758c25d4e183b4300d
Albedo <waforumo177@gmail.com>
2026-06-29 19:59:46 +0200

    sistemati import

--- HEIDI/src/app/pages/home/home/home.page.ts
+++ HEIDI/src/app/pages/home/home/home.page.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonItem, IonTitle, IonToolbar, IonIcon, IonButtons, IonMenuButton, IonCardHeader, IonCardTitle, IonSegment, IonSegmentButton, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonCardHeader, IonCardTitle, IonSegment, IonSegmentButton, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { LoginService } from 'src/app/services/auth/login.service';
-import { firstValueFrom, forkJoin, map, Observable, of, switchMap } from 'rxjs';
+import { forkJoin, map, Observable, of, switchMap } from 'rxjs';
 import { RouterModule } from '@angular/router';
-import { CalendarioComponent } from "src/app/components/calendario/calendario.component";
 import { GestionePastiService } from 'src/app/services/pasti/gestione-pasti.service';
 import { GestioneAllenamentiService } from 'src/app/services/allenamenti/gestione-allenamenti.service';
 import { Allenamento } from 'src/app/models/allenamento.model';
@@ -16,7 +15,7 @@ import { Pasto } from 'src/app/models/pasto.model';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonGrid, IonLabel, IonSegmentButton, IonSegment, IonButtons, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonItem, IonCard, IonCardContent, IonMenuButton, ReactiveFormsModule, RouterModule, CalendarioComponent, IonCardHeader, IonCardTitle]
+  imports: [IonCol, IonRow, IonGrid, IonLabel, IonSegmentButton, IonSegment, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCard, IonCardContent, IonMenuButton, ReactiveFormsModule, RouterModule, IonCardHeader, IonCardTitle]
 })
 export class HomePage implements OnInit {
 
