6e56dcb761b03631ef5c0e7f6722bffec5578bfd
Albedo <waforumo177@gmail.com>
2026-07-02 18:16:38 +0200

    prime modifiche gestione richiestte

--- HEIDI/src/app/pages/utente/richieste/richieste.page.ts
+++ HEIDI/src/app/pages/utente/richieste/richieste.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonCard } from '@ionic/angular/standalone';
 import { RichiesteUtenteComponent } from './richieste-utente/richieste-utente.component';
+import { GestioneUtentiService } from 'src/app/services/utenti/gestione-utenti.service';
 
 @Component({
   selector: 'app-richieste',
@@ -14,10 +15,17 @@ import { RichiesteUtenteComponent } from './richieste-utente/richieste-utente.co
 export class RichiestePage implements OnInit {
 
   public nuova_richiesta_pending:boolean = false;
+  public richieste_associazioni:any[] = [];
+  public richieste_voto:any[] = [];
+  public richieste_modifica:any[] = [];
 
-  constructor() { }
+  constructor(private userService:GestioneUtentiService) { }
 
   ngOnInit() {
+  }
+
+  async loadAllRichieste() {
+   
   }
 
 }
