7950e882432694c056f2a6f4d92ade1447f0fb25
Albedo <waforumo177@gmail.com>
2026-07-10 16:05:35 +0200

    il fix più potente

--- HEIDI/src/app/api-config.ts
+++ HEIDI/src/app/api-config.ts
@@ -1,6 +1,6 @@
 import { Capacitor } from '@capacitor/core';
 
-const PC_LOCAL_IP = '192.168.1.67';
+const PC_LOCAL_IP = '10.19.175.118';
 
 export const API_URL = Capacitor.isNativePlatform()
   ? `http://${PC_LOCAL_IP}:3000`
